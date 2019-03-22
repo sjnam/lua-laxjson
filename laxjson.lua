7571a46a60392f658f397aaf5f1fd51214a969e5
sjnam <jsunam@gmail.com>
2019-03-23 05:44:33 +0900

    add some comments

--- laxjson.lua
+++ laxjson.lua
@@ -56,26 +56,39 @@ enum LaxJsonError {
     LaxJsonErrorAborted
 };
 
+/* All callbacks must be provided. Return nonzero to abort the ongoing feed operation. */
 struct LaxJsonContext {
     void *userdata;
+    /* type can be property or string */
     int (*string)(struct LaxJsonContext *, enum LaxJsonType type, const char *value, int length);
+    /* type is always number */
     int (*number)(struct LaxJsonContext *, double x);
+    /* type can be true, false, or null */
     int (*primitive)(struct LaxJsonContext *, enum LaxJsonType type);
+    /* type can be array or object */
     int (*begin)(struct LaxJsonContext *, enum LaxJsonType type);
+    /* type can be array or object */
     int (*end)(struct LaxJsonContext *, enum LaxJsonType type);
+
     int line;
     int column;
+
     int max_state_stack_size;
     int max_value_buffer_size;
+
+    /* private members */
     enum LaxJsonState state;
     enum LaxJsonState *state_stack;
     int state_stack_index;
     int state_stack_size;
+
     char *value_buffer;
     int value_buffer_index;
     int value_buffer_size;
+
     unsigned int unicode_point;
     unsigned int unicode_digit_index;
+
     char *expected;
     char delim;
     enum LaxJsonType string_type;
@@ -83,8 +96,10 @@ struct LaxJsonContext {
 
 struct LaxJsonContext *lax_json_create(void);
 void lax_json_destroy(struct LaxJsonContext *context);
+
 enum LaxJsonError lax_json_feed(struct LaxJsonContext *context, int size, const char *data);
 enum LaxJsonError lax_json_eof(struct LaxJsonContext *context);
+
 const char *lax_json_str_err(enum LaxJsonError err);
 ]]
 
