442774cb7b7b5295fad137c13094448bb15c7235
sjnam <jsunam@gmail.com>
2019-03-22 23:16:45 +0900

    version up

--- laxjson.lua
+++ laxjson.lua
@@ -96,43 +96,43 @@ local other_t = ffi_typeof("int (*)(struct LaxJsonContext *, enum LaxJsonType)")
 
 
 local function default_string (ctx, jtype, value, length)
-   local type_name = jtype == C.LaxJsonTypeProperty and "property" or "string"
-   print(type_name..": "..ffi_str(value))
-   return C.LaxJsonErrorNone
+    local type_name = jtype == C.LaxJsonTypeProperty and "property" or "string"
+    print(type_name..": "..ffi_str(value))
+    return C.LaxJsonErrorNone
 end
 
 
 local function default_number (ctx, x)
-   print(x)
-   return C.LaxJsonErrorNone
+    print(x)
+    return C.LaxJsonErrorNone
 end
 
 
 local function default_primitive (ctx, jtype)
-   local type_name
-   if jtype == C.LaxJsonTypeTrue then
-      type_name = "true"
-   elseif jtype == C.LaxJsonTypeFalse then
-      type_name = "false"
-   else
-      type_name = "null"
-   end
-   print("primitive: "..type_name)
-   return C.LaxJsonErrorNone
+    local type_name
+    if jtype == C.LaxJsonTypeTrue then
+        type_name = "true"
+    elseif jtype == C.LaxJsonTypeFalse then
+        type_name = "false"
+    else
+        type_name = "null"
+    end
+    print("primitive: "..type_name)
+    return C.LaxJsonErrorNone
 end
 
 
 local function default_begin (ctx, jtype)
-   local type_name = jtype == C.LaxJsonTypeArray and "array" or "object"
-   print("begin "..type_name)
-   return C.LaxJsonErrorNone
+    local type_name = jtype == C.LaxJsonTypeArray and "array" or "object"
+    print("begin "..type_name)
+    return C.LaxJsonErrorNone
 end
 
 
 local function default_end (ctx, jtype)
-   local type_name = jtype == C.LaxJsonTypeArray and "array" or "object"
-   print("end "..type_name)
-   return C.LaxJsonErrorNone
+    local type_name = jtype == C.LaxJsonTypeArray and "array" or "object"
+    print("end "..type_name)
+    return C.LaxJsonErrorNone
 end
 
 
@@ -140,7 +140,7 @@ local laxjson = ffi_load "laxjson"
 
 
 local _M = {
-   version = "0.0.1"
+    version = "0.2.8"
 }
 
 
@@ -148,16 +148,17 @@ local mt = { __index = _M }
 
 
 function _M.new (o)
-   local o = o or {}
-   local ctx = laxjson.lax_json_create()
-   ctx[0].userdata = o.userdata or NULL
-   ctx[0].string = ffi_cast(string_t, o.fn_string or default_string)
-   ctx[0].number = ffi_cast(number_t, o.fn_number or default_number)
-   ctx[0].primitive = ffi_cast(other_t, o.fn_primitive or default_primitive)
-   ctx[0].begin = ffi_cast(other_t, o.fn_begin or default_begin)
-   ctx[0]["end"] = ffi_cast(other_t, o.fn_end or default_end)
+    local o = o or {}
 
-   return setmetatable({ ctx = ctx }, mt)
+    local ctx = laxjson.lax_json_create()
+    ctx[0].userdata = o.userdata or NULL
+    ctx[0].string = ffi_cast(string_t, o.fn_string or default_string)
+    ctx[0].number = ffi_cast(number_t, o.fn_number or default_number)
+    ctx[0].primitive = ffi_cast(other_t, o.fn_primitive or default_primitive)
+    ctx[0].begin = ffi_cast(other_t, o.fn_begin or default_begin)
+    ctx[0]["end"] = ffi_cast(other_t, o.fn_end or default_end)
+
+    return setmetatable({ ctx = ctx }, mt)
 end
 
 
@@ -167,53 +168,53 @@ end
 
 
 function _M:set_fn_string (fn)
-   self.ctx[0].string = ffi_cast(string_t, fn)
+    self.ctx[0].string = ffi_cast(string_t, fn)
 end
 
 
 function _M:set_fn_number (fn)
-   self.ctx[0].number = ffi_cast(number_t, fn)
+    self.ctx[0].number = ffi_cast(number_t, fn)
 end
 
 
 function _M:set_fn_primitive (fn)
-   self.ctx[0].primitive = ffi_cast(other_t, fn)
+    self.ctx[0].primitive = ffi_cast(other_t, fn)
 end
 
 
 function _M:set_fn_begin (fn)
-   self.ctx[0].begin = ffi_cast(other_t, fn)
+    self.ctx[0].begin = ffi_cast(other_t, fn)
 end
 
 
 function _M:set_fn_end (fn)
-   self.ctx[0]["end"] = ffi_cast(other_t, fn)
+    self.ctx[0]["end"] = ffi_cast(other_t, fn)
 end
 
 
 function _M:feed (amt_read, buf)
-   local err = laxjson.lax_json_feed(self.ctx, amt_read, buf)
-   self.line = self.ctx[0].line
-   self.column = self.ctx[0].column
-   return err
+    local err = laxjson.lax_json_feed(self.ctx, amt_read, buf)
+    self.line = self.ctx[0].line
+    self.column = self.ctx[0].column
+    return err
 end
 
 
 function _M:eof ()
-   local err = laxjson.lax_json_eof(self.ctx)
-   self.line = self.ctx[0].line
-   self.column = self.ctx[0].column
-   return err
+    local err = laxjson.lax_json_eof(self.ctx)
+    self.line = self.ctx[0].line
+    self.column = self.ctx[0].column
+    return err
 end
 
 
 function _M:str_err (err)
-   return ffi_str(laxjson.lax_json_str_err(err))
+    return ffi_str(laxjson.lax_json_str_err(err))
 end
 
 
 function _M:free ()
-   laxjson.lax_json_destroy(self.ctx);
+    laxjson.lax_json_destroy(self.ctx);
 end
 
 
