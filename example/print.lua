0e774d19b174ae9c77b61172fff6228087d80561
sjnam <jsunam@gmail.com>
2019-03-26 08:43:55 +0900

    global indent

--- example/print.lua
+++ example/print.lua
@@ -2,16 +2,10 @@ local ffi = require "ffi"
 local C = ffi.C
 local laxjson = require "laxjson"
 
-local function mydata (ctx, n)
-    local v = ffi.cast("int*", ctx.userdata)
-    if not n then return v[0] end
-    v[0] = n
-end
+local indent = 0
 
 local laxj = laxjson.new {
-    userdata = ffi.new("int[1]", 0),
     on_string = function (ctx, jtype, value, length)
-        local indent = mydata(ctx)
         if jtype == C.LaxJsonTypeProperty then
             io.write(string.rep(" ", indent+1))
         end
@@ -34,16 +28,14 @@ local laxj = laxjson.new {
         return 0
     end,
     on_begin = function (ctx, jtype)
-        local indent = mydata(ctx)
         io.write(string.rep(" ", indent))
         print(jtype == C.LaxJsonTypeArray and "[" or "{")
-        mydata(ctx, indent+1)
+        indent = indent + 1
         return 0
     end,
     on_end = function (ctx, jtype)
-        local indent = mydata(ctx)
-        mydata(ctx, indent-1)
-        io.write(string.rep(" ", indent-1))
+        indent = indent - 1
+        io.write(string.rep(" ", indent))
         print(jtype == C.LaxJsonTypeArray and "]" or "}")
         return 0
     end
