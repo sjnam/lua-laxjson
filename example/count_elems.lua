b7395eb1868f9bb3d538aac801c1fbcbb84b987d
sjnam <jsunam@gmail.com>
2019-03-26 22:06:00 +0900

    count array elems

--- example/count_elems.lua
+++ example/count_elems.lua
@@ -3,34 +3,20 @@ local C = ffi.C
 local laxjson = require "laxjson"
 
 
-ffi.cdef[[
-typedef struct {
-    uint8_t on_arr, count;
-} mydata_t;
-]]
-
-
-local userdata = ffi.new("mydata_t[1]")
-
-local function mydata (ctx)
-    return ffi.cast("mydata_t*", ctx.userdata)
-end
-
+local on_arr, count = false, 0
 
 local laxj = laxjson.new {
-    userdata = userdata,
     on_begin = function (ctx, jtype)
-        local data = mydata(ctx)
         if jtype == C.LaxJsonTypeArray then
-            mydata(ctx).on_arr = 1
-        elseif data.on_arr == 1 then
-            mydata(ctx).count = data.count + 1
+            on_arr = true
+        elseif on_arr then
+            count = count + 1
         end
         return 0
     end,
     on_end = function (ctx, jtype)
         if jtype == C.LaxJsonTypeArray then
-            mydata(ctx).on_arr = 0
+            on_arr = false
         end
         return 0
     end
@@ -40,5 +26,5 @@ local ok, l, col, err = laxj:parse("array.json")
 if not ok then
     print("Line "..l..", column "..col..": "..err)
 else
-    print("# of arrayelems: "..userdata[0].count)
+    print("# of arrayelems: "..count)
 end
